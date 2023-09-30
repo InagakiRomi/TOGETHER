d41461c862f6ff487100a6df5bbd6a2c085295ef
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2023-09-30 18:11:18 +0800

    升級Unity2022.3.10.f1

--- TOGETHER/Assets/Scripts/Goal.cs
+++ TOGETHER/Assets/Scripts/Goal.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
@@ -13,11 +12,18 @@ public class Goal : MonoBehaviour
     private PlayTag PlayTag;
     private GameAudio SE;
 
+
     void Start()
     {
         GM = GameObject.Find("GM").GetComponent<GM>();
         PlayTag = GameObject.Find("GM").GetComponent<PlayTag>();
-        SE = GameObject.Find("SE").GetComponent<GameAudio>();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if(sceneName == "Level02")
+        {
+            SE = GameObject.Find("SE").GetComponent<GameAudio>();
+        }
     }
 
     void HitByRaycast() //被射線打到時會進入此方法
